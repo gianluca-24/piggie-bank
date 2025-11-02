af92b88352efe8a0e593c2e75ab8e6192e1d3e75
gianluca-24 <78686194+gianluca-24@users.noreply.github.com>
2025-11-02 18:37:04 +0100

    pigggie db

--- database/aux/connection.py
+++ database/aux/connection.py
@@ -1,7 +1,7 @@
 import os
 import sqlite3
 
-def get_connection(db_name="app_database.db"):
+def get_connection(db_name="piggie.db"):
     """Return a connection to the database located in the project root."""
     root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     db_path = os.path.join(root_dir, db_name)
